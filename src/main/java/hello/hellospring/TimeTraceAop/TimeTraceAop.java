ccd8d7b4363fe402961768bb85e9f64dbbed4aec
Gunwoong-Heo <gunwoong1129@gmail.com>
2021-12-05 00:39:04 +0900

    TimeTraceAop 추가

---
+++ src/main/java/hello/hellospring/TimeTraceAop/TimeTraceAop.java
@@ -0,0 +1,24 @@
+package hello.hellospring.TimeTraceAop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component  // 특별한 상황에 사용되는 것 이기 때문에, Component 스캔 방식보다도 SpringConfig 파일에 @Bean으로 등록하여 사용하는 경우가 많음.
+public class TimeTraceAop {
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+
+}
