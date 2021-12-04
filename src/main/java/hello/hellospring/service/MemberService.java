b6c738477c3b654301fb39ae3ebd283746ac00f8
Gunwoong-Heo <gunwoong1129@gmail.com>
2021-12-04 23:35:46 +0900

    @Transactional 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,14 +2,13 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 //@Service
+@Transactional  // JPA를 쓸때는 항상 있어야 함. (select 시에는 필요 X, 여기서는 join메소드에만 걸어도 됨.)
 public class MemberService {
 
     private final MemberRepository memberRepository;
